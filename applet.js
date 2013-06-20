6e30155e30f7f2a107e54dd563479d0f10bb20d5
Stephen Collins <scrivera64@gmail.com>
2013-06-20 00:16:36 -0600

    Add visual feedback and other tweaks

--- applet.js
+++ applet.js
@@ -1,11 +1,16 @@
 const Applet = imports.ui.applet;
 const Settings = imports.ui.settings;
+const Tweener = imports.ui.tweener;
+const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
+const St = imports.gi.St;
 const Util = imports.misc.util;
 const Lang = imports.lang;
+const Mainloop = imports.mainloop;
 
 
 const UUID = "commandLauncher@scollins";
+const ICON_HEIGHT = 22;
 
 
 function MyApplet(orientation, panel_height, instanceId) {
@@ -18,8 +23,9 @@ MyApplet.prototype =  {
     _init: function(orientation, panel_height, instanceId) {
         try {
             this.orientation = orientation;
-            this._instanceId = instanceId;
-            Applet.TextIconApplet.prototype._init.call(this, this.orientation, panel_height);
+            this.panel_height = panel_height;
+            this.instanceId = instanceId;
+            Applet.IconApplet.prototype._init.call(this, this.orientation, panel_height);
             
             this._bind_settings();
             
@@ -36,6 +42,10 @@ MyApplet.prototype =  {
     
     on_applet_clicked: function(event) {
         try {
+            let allocation = this._applet_icon_box.get_allocation_box();
+            this._applet_icon_box.width = allocation.x2 - allocation.x1;
+            this._applet_icon_box.height = allocation.y2 - allocation.y1;
+            this._animate(3);
             if ( this.command == "" ) this._open_settings();
             else Util.spawnCommandLine(this.command);
         } catch(e) {
@@ -44,7 +54,7 @@ MyApplet.prototype =  {
     },
     
     _bind_settings: function () {
-        this.settings = new Settings.AppletSettings(this, UUID, this._instanceId);
+        this.settings = new Settings.AppletSettings(this, UUID, this.instanceId);
         this.settings.bindProperty(Settings.BindingDirection.IN, "icon", "icon", this._set_icon);
         this.settings.bindProperty(Settings.BindingDirection.IN, "command", "command", function(){});
         this.settings.bindProperty(Settings.BindingDirection.IN, "description", "description", this._set_tooltip);
@@ -56,11 +66,45 @@ MyApplet.prototype =  {
     
     _set_icon: function() {
         if ( this.icon.split("/").length > 1 ) this.set_applet_icon_path(this.icon);
-        else this.set_applet_icon_name(this.icon);
+        else {
+            this._applet_icon = St.TextureCache.get_default().load_gicon(null, new Gio.ThemedIcon({ name: this.icon }), ICON_HEIGHT);
+            this._applet_icon_box.child = this._applet_icon;
+            this.__icon_type = -1;
+            this.__icon_name = this.icon;
+        }
     },
     
     _set_tooltip: function() {
         this.set_applet_tooltip(this.description);
+    },
+    
+    locate: function() {
+        let box = this.actor;
+        box.add_style_pseudo_class("highlight");
+        Mainloop.timeout_add(3000, function() { box.remove_style_pseudo_class("highlight"); });
+    },
+    
+    _animate:function(count) {
+        if ( count < 1 ) return;
+        Tweener.addTween(this._applet_icon, {
+            width: ICON_HEIGHT * .8,
+            height: ICON_HEIGHT * .8,
+            time: 0.2,
+            transition: 'easeOutQuad',
+            onComplete: function() {
+                Tweener.addTween(this._applet_icon, {
+                    width: ICON_HEIGHT,
+                    height: ICON_HEIGHT,
+                    time: 0.2,
+                    transition: 'easeOutQuad',
+                    onComplete: function() {
+                        this._animate(count-1);
+                    },
+                    onCompleteScope: this
+                });
+            },
+            onCompleteScope: this
+        });
     }
 }
 
